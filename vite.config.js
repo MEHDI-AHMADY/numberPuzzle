10e36a50909a3c97010e3f3432973050ad4c23f0
Mehdi Ahmadi <metti799@gmail.com>
2024-10-30 14:27:46 +0330

    feat : made it pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "number puzzle",
+        short_name: "numberPuzzle",
+        description: "a simple number puzzle",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/images/puzzleIcon192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/puzzleIcon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
